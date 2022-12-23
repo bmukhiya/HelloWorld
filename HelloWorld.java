612d13b97fb405d095736355d7d46a8bcaf027da
bmukhiya <96289678+bmukhiya@users.noreply.github.com>
2022-12-23 12:27:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 class HelloWorld{  
+    String password="pass";
     public static void main(String args[]){  
-     System.out.println("Hello World");  
+     System.out.println("Hello World"); 
+     System.out.println(password);
+        
     }  
 }  
